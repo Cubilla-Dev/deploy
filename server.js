7808486f905d40ae05a865199119f1a71f05e8be
Cubilla <cubilladev@gmail.com>
2023-10-31 18:39:36 -0300

    Cambios para hacer rollback

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
   res.json({message: 'Funcionando'})
 })
 
+app.get('/prueba', (req, res) => {
+    res.json({message: 'Practica de un rollback'})
+  })
+
 app.listen(3000, console.log('Server en linea'))
